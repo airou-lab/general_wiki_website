b6740474906c689671b8309d317c377f272133ca
arika <arika.khor@gmail.com>
2026-08-22 14:36:14 -0500

    fix: inject missing emitter plugins into quartz build to ensure PDFs are copied

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,17 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import { Assets } from "./quartz/plugins/emitters/assets"
+import { Static } from "./quartz/plugins/emitters/static"
+import { ComponentResources } from "./quartz/plugins/emitters/componentResources"
 
 const config = await loadQuartzConfig()
+
+if (!config.plugins.emitters) {
+    config.plugins.emitters = []
+}
+// Force inject missing emitters because YAML config doesn't load them properly
+config.plugins.emitters.push(Assets())
+config.plugins.emitters.push(Static())
+config.plugins.emitters.push(ComponentResources())
+
 export default config
 export const layout = await loadQuartzLayout()
